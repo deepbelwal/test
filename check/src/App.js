e05d0b9f106e7a4cfae0f8eb210e0cda065ad137
parvathi9964 <parvathi9964@gmail.com>
2019-03-05 10:38:56 +0530

    First Commit

---
+++ check/src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import './App.css';
+import del from './del.jpg';
+
+class App extends Component {
+  render() {
+    return (
+
+    <div>
+    <div class="column ">
+    <h2>Nineleaps</h2>
+    <p>checklist management</p>
+    </div>
+
+    <div class="row" >
+    <input type="button" className="but" value="+"></input>
+    </div>  
+
+    <div class="row1" >
+    <img src={del} className="im" alt="Profile image" onClick=" "></img>
+    </div>
+
+    <div class="row2" >
+    <input type="text" value=""></input>
+    <input type="button" className="buton" value="search"></input> </div>
+    <div class="row3">
+    <input type="button" className="butt" value="P"></input></div>
+
+    <div class="column1 "></div>
+    <div class="row4" ></div>
+    <div class="column1 "></div>
+    <div class="row5"></div>
+    <div class="column3 "></div>
+    <div class="row6"></div>
+   
+    
+    </div>
+
+
+    );
+  }
+}
+
+export default App;
